70afa74fcd43c224f6098a6cb8b736f909a05720
getssh <gtamrat33@gmail.com>
2023-03-10 01:36:28 +0300

    Update addNewScore function to refresh new scores

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,7 +1,15 @@
 const apiEndPoint = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
 const gameId = 'GlLBSxZJ2Lt3ESEwpZKy';
 
-export const addNewScore = async (userName, socreValue) => {
+export const getScores = async () => {
+  const getScores = await fetch(`${apiEndPoint}/${gameId}/scores`);
+  const scores = await getScores.json();
+  const scoreResult = scores.result;
+
+  return scoreResult;
+};
+
+export const addNewScore = async (userName, socreValue, refreshScore) => {
   let postData;
   try {
     postData = await fetch(`${apiEndPoint}/${gameId}/scores`, {
@@ -20,19 +28,10 @@ export const addNewScore = async (userName, socreValue) => {
 
   if (postData.status === 201) {
     const response = await postData.json();
+    refreshScore();
     console.log(response.status);
-    window.location.reload();
   } else {
     const errorText = await postData.json();
     console.log('File creation failed', postData.status, errorText.message);
   }
-};
-
-export const getScores = async () => {
-  const getScores = await fetch(`${apiEndPoint}/${gameId}/scores`);
-  const scores = await getScores.json();
-  const scoreResult = scores.result;
-
-  console.log(scoreResult);
-  return scoreResult;
 };
