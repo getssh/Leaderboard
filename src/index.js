063e9321050521bb7a30a278ad2369dcfa546d33
getssh <gtamrat33@gmail.com>
2023-03-09 18:51:49 +0300

    Add api endpoint and get, post methods to get scores

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style.css';
+
+const gameId = '4mvJYTSh7H5F6fBw4moX';
+
+const addNewScore = async (userName, socreValue) => {
+  const postData = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: userName,
+      score: socreValue,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  const response = await postData.json();
+  console.log(response);
+};
+
+const getScores = async () => {
+  const getScores = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`);
+  const scores = await getScores.json();
+
+  console.log(scores);
+  console.log(scores.result.length);
+};
+
+addNewScore('John Doe', 42);
+getScores();
